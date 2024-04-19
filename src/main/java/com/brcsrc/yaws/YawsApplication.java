430840221c0161eb998a8fc0a6a09df998d6a8c7
bryson crandall <bryson.crandall@yahoo.com>
2024-04-19 00:41:05 -0600

    initial, bootstrap spring

---
+++ src/main/java/com/brcsrc/yaws/YawsApplication.java
@@ -0,0 +1,13 @@
+package com.brcsrc.yaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YawsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YawsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brcsrc/yaws/YawsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brcsrc.yaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YawsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

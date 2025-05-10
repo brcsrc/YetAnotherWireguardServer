3a30c5098febfe765bdc3c9907905095eb71b8e2
Jon <quiet_wizards@icloud.com>
2025-05-10 15:10:04 -0600

    Login / Dashboard / Theme with Cloudscape Shell (#35)

---
+++ yaws-frontend/src/types/network.ts
@@ -0,0 +1,9 @@
+export interface Network {
+  networkName: string;
+  networkCidr: string;
+  networkListenPort: number;
+  networkPrivateKeyName: string;
+  networkPublicKeyName: string;
+  networkTag: string;
+  networkStatus: string;
+}

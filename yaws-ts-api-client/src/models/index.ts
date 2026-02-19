78aff88fe4733acea0bf5cfacfbda34d066ef63b
Cody Edwards <coed@Codys-MacBook-Pro.local>
2026-02-19 00:23:46 -0700

    Add TOTP 2FA Option - autogenerated

--- yaws-ts-api-client/src/models/index.ts
+++ yaws-ts-api-client/src/models/index.ts
@@ -1,5 +1,7 @@
 /* tslint:disable */
 /* eslint-disable */
+export * from './AuthenticateStartRequest';
+export * from './AuthenticateStartResponse';
 export * from './Client';
 export * from './CreateNetworkClientRequest';
 export * from './GetNextAvailableClientAddressResponse';
@@ -10,9 +12,14 @@ export * from './ListNetworksRequest';
 export * from './ListNetworksResponse';
 export * from './Network';
 export * from './NetworkClient';
+export * from './RegenerateRecoveryCodesResponse';
 export * from './SseEmitter';
 export * from './StreamClientConnectionInfoRequest';
 export * from './StreamNetworkConnectionInfoRequest';
+export * from './TotpEnrollConfirmRequest';
+export * from './TotpEnrollStartResponse';
 export * from './UpdateNetworkRequest';
 export * from './User';
+export * from './VerifyRecoveryCodeRequest';
+export * from './VerifyTotpRequest';
 export * from './WhoamiResponse';
